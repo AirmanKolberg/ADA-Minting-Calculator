9f3c6149188a298694287d3cdf1e5da63bb51bb6
T.J. Kolberg <tkolberg@asu.edu>
2021-07-13 16:04:50 -0600

    added initial data_updater.py file

---
+++ data_updater.py
@@ -0,0 +1,14 @@
+from time import sleep
+from system_functions import bash_command
+
+"""
+This application can be used to automate the process
+of updating the data.json file to GitHub when new
+information is obtain in an effort to stay up-to-date
+with the data I'm sharing.
+"""
+
+
+if __name__ == '__main__':
+
+    print('code this')
