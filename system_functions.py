adcda85b1ed97a0f0a509108aa979171351d155f
T.J. Kolberg <tkolberg@asu.edu>
2021-07-13 23:39:32 -0600

    ⚙  General Functions

--- system_functions.py
+++ system_functions.py
@@ -1,4 +1,5 @@
 from os import system
+from time import sleep
 
 
 def bash_command(user_in):
@@ -22,6 +23,42 @@ def verify_yes_or_no(response):
 
     else:
         return verify_yes_or_no(input(f"{response} is neither 'yes' nor 'no', please try again: ").lower())
+
+
+def countdown(seconds):
+    
+    # Count down until 0 so that the last second is counted
+    while seconds > -1:
+
+        mins, secs = divmod(seconds, 60)
+
+        # Setup grammar conditions for min/mins
+        if mins != 1:
+            min_display = 'mins'
+
+        else:
+            min_display = 'min'
+
+        # Setup grammar conditions for sec/secs
+        if secs != 1:
+            sec_display = 'secs'
+
+        else:
+            sec_display = 'sec'
+
+        # `timer` example: "Updating in 121mins 1sec..."
+        timer = f'Updating in {mins}{min_display} {secs}{sec_display}...'
+
+
+        print(timer, end="\r")
+
+        # Wait a second
+        sleep(1)
+
+        # Take a second away from the total
+        seconds -= 1
+
+    clear_screen()
 
 
 if __name__ == '__main__':
