d8d258cff47811ea21b64ae0286a429a942e6fd8
T.J. Kolberg <tkolberg@asu.edu>
2021-07-13 21:42:21 -0600

    ⚙ General Functions

--- system_functions.py
+++ system_functions.py
@@ -12,5 +12,14 @@ def clear_screen():
     bash_command('clear')
 
 
+def verify_yes_or_no(response):
+    if response == 'yes':
+        return True
+    elif response == 'no':
+        return False
+    else:
+        return verify_yes_or_no(input(f"{response} is neither 'yes' nor 'no', please try again: ").lower())
+
+
 if __name__ == '__main__':
     pass
