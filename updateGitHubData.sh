20390a07fa665851202b8aedd6c4a7103bd71d8e
T.J. Kolberg <tkolberg@asu.edu>
2021-07-16 18:51:00 -0600

    🦾  Another Helper Robot

--- updateGitHubData.sh
+++ updateGitHubData.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-git status
-
-# Wait five seconds for user's viewing pleasure
-sleep 5
-
 git add data.json
 git commit -m "💽 Freebee Starting Data (from my server)"
 git push https://github.com/AirmanKolberg/ADA-Minting-Calculator.git
