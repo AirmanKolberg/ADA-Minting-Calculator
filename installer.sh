26b36e05d2a94d624c5054285d8c8950958f3da9
T.J. Kolberg <tkolberg@asu.edu>
2021-07-24 21:57:29 -0600

    🛠   Application Installer (Execute First)

--- installer.sh
+++ installer.sh
@@ -5,3 +5,9 @@ mv estimations_log_example.json estimations_log.json
 
 # Install all required dependencies
 pip3 install -r requirements.txt
+
+clear
+echo "Everything installed and ready to go!"
+sleep 2
+clear
+exit
